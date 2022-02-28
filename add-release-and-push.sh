f7d9e38fb0ede0937b58241f325781ab7ff1c2a9
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2022-02-28 01:07:14 +0100

    Modified release script

--- add-release-and-push.sh
+++ add-release-and-push.sh
@@ -1,17 +1,4 @@
-#!/usr/bin/env bash
-
-#------------------------------------------------------------------------------
-# Adds a nightly binary to a local checkout of solc-bin repository, updates
-# file lists, commits and pushes the changes to the remote repository.
-#
-# The script expects to find the working copy in its working directory with
-# the right branch already checked out and all the details necessary to make
-# a commit and push it (committer name, `origin` remote, etc.) configured.
-#
-# The script fails if the specified version is already present in the
-# repository.
-#------------------------------------------------------------------------------
-
+#!/bin/bash
 set -e
 
 die() {
@@ -21,7 +8,8 @@ die() {
 
 release_firmware="$1"
 release_version="$2"
-(( $# == 2 )) || die "Expected exactly 2 parameters."
+token="$3"
+(( $# == 2 || $# == 3 )) || die "Expected 2 or 3 parameters."
 
 if [[ $(git status --porcelain) != "" ]]; then
     >&2 git status
@@ -32,7 +20,6 @@ echo "Adding hp100/hp100-firmware-v${release_version}.hex"
 [[ ! -e "hp100/hp100-firmware-v${release_version}.hex" ]] || die "File hp100/hp100-firmware-v${release_version}.hex already exists."
 cp "$release_firmware" "hp100/hp100-firmware-v${release_version}.hex"
 
-# Stage the new nightly before running the list update to be able to detect any unintended changes caused by the script.
 git add --verbose "hp100/hp100-firmware-v${release_version}.hex"
 node ./update-manifest.js
 git add --verbose hp100/manifest.json
@@ -44,4 +31,8 @@ if [[ $(git status --porcelain) != "" ]]; then
     die "Unexpected file modifications found. This should never happen and might be the result of a bug."
 fi
 
-git push origin HEAD
+if [[ $token == "" ]]; then
+    git push origin HEAD
+else
+    git push "https://$token@github.com/helvest-systems-gmbh/releases.git"
+fi
